bb25e2b14725026144b942b517767c5acfa3fd41
CesarChe <cesar_leo_leo@hotmail.com>
2024-06-11 20:33:59 -0500

    add wppweb v1

--- src/shared/process.message.js
+++ src/shared/process.message.js
@@ -1,78 +1,78 @@
 const whatsappService = require('../services/whatsapp.service');
 const chatGptService = require('../services/ia.service');
-const dbService = require('../services/db.service');
+// const dbService = require('../services/db.service');
 const utilityService = require('../services/utility.service');
 
-async function processByCliente(text, number , ia = 'Gemini') {
+// async function processByCliente(text, number , ia = 'Gemini') {
 
-    let response = null;
-    let cliente = null;
-    let procesoRegistro = false;
+//     let response = null;
+//     let cliente = null;
+//     let procesoRegistro = false;
 
-    try {
-        console.log(await dbService.getAllClients());
-        cliente = await dbService.getClientByPhone(number);
-    } catch (error) {
-        console.error(error);
-    };
+//     try {
+//         console.log(await dbService.getAllClients());
+//         cliente = await dbService.getClientByPhone(number);
+//     } catch (error) {
+//         console.error(error);
+//     };
  
 
-    // consultamos a gemini
-    if(ia == 'Gemini'){
-        // response =  await chatGptService.getMessageGemini(text)
-        response = await chatGptService.getMessageGeminiInstance_(text);
+//     // consultamos a gemini
+//     if(ia == 'Gemini'){
+//         // response =  await chatGptService.getMessageGemini(text)
+//         response = await chatGptService.getMessageGeminiInstance_(text);
 
-        response = response && response.includes('N0L0S3') ? "Este problema se le derivara a un desarrollador." : response;
-    };
+//         response = response && response.includes('N0L0S3') ? "Este problema se le derivara a un desarrollador." : response;
+//     };
 
-    // consultamos a gpt
-    if(ia == 'GPT'){
-        response = chatGptService.getMessageChatGPT(text)
-    };
+//     // consultamos a gpt
+//     if(ia == 'GPT'){
+//         response = chatGptService.getMessageChatGPT(text)
+//     };
 
-    response = response ? response : "No se pudo obtener respuesta , vuelva a intentarlo";
-    if(cliente){
-        // Seguimos el flujo de conversacion pedimos el nombre y luego dni
+//     response = response ? response : "No se pudo obtener respuesta , vuelva a intentarlo";
+//     if(cliente){
+//         // Seguimos el flujo de conversacion pedimos el nombre y luego dni
 
-        // validamos si el cliente ya tiene nombre
-        if(!cliente.cliente_nombre){
-            procesoRegistro = true;
-            if(utilityService.esNombreValido(text)){
-                await dbService.updateClient(cliente.cliente_id, { ...cliente , cliente_nombre: text});
-                response = "Hola "+text+" , ahora necesito tu DNI.";
-            }else{
-                response = "Nombre invalido, por favor ingrese un nombre valido.";
-            };
-        };
+//         // validamos si el cliente ya tiene nombre
+//         if(!cliente.cliente_nombre){
+//             procesoRegistro = true;
+//             if(utilityService.esNombreValido(text)){
+//                 await dbService.updateClient(cliente.cliente_id, { ...cliente , cliente_nombre: text});
+//                 response = "Hola "+text+" , ahora necesito tu DNI.";
+//             }else{
+//                 response = "Nombre invalido, por favor ingrese un nombre valido.";
+//             };
+//         };
 
-        // validamos si el cliente ya tiene dni
-        if(!cliente.cliente_dni && cliente.cliente_nombre){
-            procesoRegistro = true;
-            if(utilityService.esDniValido(text)){
-                await dbService.updateClient(cliente.cliente_id, { ...cliente , cliente_dni: text});
-                response = "¡Muchas gracias "+cliente.cliente_nombre+" , ahora puedo ayudarte con tu consulta!.";
-            }else{
-                response = "DNI invalido, por favor ingrese un DNI valido.";
-            };
-        };
+//         // validamos si el cliente ya tiene dni
+//         if(!cliente.cliente_dni && cliente.cliente_nombre){
+//             procesoRegistro = true;
+//             if(utilityService.esDniValido(text)){
+//                 await dbService.updateClient(cliente.cliente_id, { ...cliente , cliente_dni: text});
+//                 response = "¡Muchas gracias "+cliente.cliente_nombre+" , ahora puedo ayudarte con tu consulta!.";
+//             }else{
+//                 response = "DNI invalido, por favor ingrese un DNI valido.";
+//             };
+//         };
 
-        // guardamos la conversacion
-        await dbService.createConversation({ clienteId: cliente.cliente_id , conversacion_mensaje: text});
+//         // guardamos la conversacion
+//         await dbService.createConversation({ clienteId: cliente.cliente_id , conversacion_mensaje: text});
 
-        // enviamos mensaje
-        if(procesoRegistro)  whatsappService.sendMessageWhatsap(response, number);
-        else whatsappService.sendMessageListWhatsap(response, number);
+//         // enviamos mensaje
+//         if(procesoRegistro)  whatsappService.sendMessageWhatsap(response, number);
+//         else whatsappService.sendMessageListWhatsap(response, number);
 
-    }else{
+//     }else{
 
-        // Iniciamos creando nuestro cliente
-        cliente = await dbService.createClient({cliente_telefono: number});
-        console.log('cliente creado:', cliente);
-        await dbService.createConversation({ clienteId: cliente.cliente_id , conversacion_mensaje: text,});
-        whatsappService.sendMessageWhatsap(response, number);
-    };
+//         // Iniciamos creando nuestro cliente
+//         cliente = await dbService.createClient({cliente_telefono: number});
+//         console.log('cliente creado:', cliente);
+//         await dbService.createConversation({ clienteId: cliente.cliente_id , conversacion_mensaje: text,});
+//         whatsappService.sendMessageWhatsap(response, number);
+//     };
 
-};
+// };
 
 async function process(text, number , ia = 'Gemini') {
     let response = null;
@@ -96,5 +96,5 @@ async function process(text, number , ia = 'Gemini') {
 
 module.exports = {
     process,
-    processByCliente
+    // processByCliente
 };
