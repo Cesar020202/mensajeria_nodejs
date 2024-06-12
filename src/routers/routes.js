be2b29bf18eb030ab8844b1803685a274b309322
CesarChe <cesar_leo_leo@hotmail.com>
2024-06-11 20:41:03 -0500

    fix v1

--- src/routers/routes.js
+++ src/routers/routes.js
@@ -1,8 +1,32 @@
 const express = require('express');
+const { Client } = require('whatsapp-web.js');
 
 const router = express.Router();
 const whatsappController = require('../controllers/whatsapp.controller');
-const whatsappWebController = require('../services/whatsappWeb.service');
+// const whatsappWebController = require('../services/whatsappWeb.service');
+
+
+
+const client = new Client();
+
+client.on('qr', (qr) => {
+    // Generate and scan this code with your phone
+    console.log('QR RECEIVED', qr);
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.on('message', msg => {
+    console.log(msg);
+    if (msg.body == '!ping') {
+        msg.reply('pong');
+    }
+});
+
+client.initialize();
+
 
 router
 .get('/', whatsappController.verifyToken)
