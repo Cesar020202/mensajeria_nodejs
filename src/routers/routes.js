95b48f11ddddf446096bc7159044d56e554d918a
CesarChe <cesar_leo_leo@hotmail.com>
2024-06-11 20:26:21 -0500

    add wppweb v1

--- src/routers/routes.js
+++ src/routers/routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 const whatsappController = require('../controllers/whatsapp.controller');
+const whatsappWebController = require('../services/whatsappWeb.service');
 
 router
 .get('/', whatsappController.verifyToken)
